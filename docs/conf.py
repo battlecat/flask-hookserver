cc4e1d70b67043fc15e994b433bf5b75362e006d
Nick Frost <nickfrostatx@gmail.com>
2016-01-02 20:20:13 -0600

    Update docs configuration

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ import shlex
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.abspath('_themes'))
 
 # -- General configuration ------------------------------------------------
@@ -293,5 +293,6 @@ texinfo_documents = [
 
 intersphinx_mapping = {
     'http://docs.python.org/': None,
+    'http://flask.pocoo.org/docs/': None,
     'http://werkzeug.pocoo.org/docs/': None,
 }
