672a1df34462de3001596e7f281f8c2f3e2297b7
battlecat <17527695@qq.com>
2016-12-09 11:06:41 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask.ext.hookserver import Hooks
+
+app = Flask(__name__)
+app.config['GITHUB_WEBHOOKS_KEY'] = 'my_secret_key'
+
+hooks = Hooks(app, url='/hooks')
+
+@hooks.hook('ping')
+def ping(data, guid):
+    return 'pong'
+
+app.run()
